f20f00f997e631aeb4108cb069775a0ce9876258
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-07-04 11:49:43 -0700

    initial commit: Challenge 1, Challenge 2 started

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,31 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("WDIController", WDIController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController() {
+  this.full_name = "Vanessa Farias";
+  this.age = 21;
+  this.city = "San Francisco Bay Area";
+  this.state = "California";
+  this.letters = 13;
+  this.shout  = function(str) {
+  	return str.toUpperCase() + "!";
+  	// Where is 'str' coming from
+  };
+}
+
+// Exercise # 2 
+
+function WDIController() {
+  this.class_name = "WDI-29";
+  this.enrolled_students = 21;
+  // this.city = "San Francisco Bay Area";
+  // this.state = "California";
+  // this.letters = 13;
+  // this.shout  = function(str) {
+  // 	return str.toUpperCase() + "!";
+  // 	// Where is 'str' coming from
+  // };
+}
