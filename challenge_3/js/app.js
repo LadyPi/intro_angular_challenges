498b445732a6afbc08de92fd252e70ba4e55adbe
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-07-05 07:51:01 -0700

    adds challenge 3 edits

--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,18 @@
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
+app.controller("infoController", infoController);
+app.controller("loanController", loanController);
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+function infoController(){
+  this.name = "Jon Doe"; // default name
+  this.info.height = 5.6; // default height in feet
+}
+
+function loanController() {
+	this.loan.amount = 12.00;
+	this.loan.apr = 2;
+	this.loan.month = 12;
+	this.accrued_interest = loan.apr * loan.amount;
+	this.total_balance = loan.amount + accrued_interest;
+}
