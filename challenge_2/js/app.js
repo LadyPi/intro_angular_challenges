6a14b97c799e5b29123e721a2dad385476affada
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-07-05 06:58:08 -0700

    Completing challenge 2

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -20,12 +20,15 @@ function WelcomeController() {
 
 function WDIController() {
   this.class_name = "WDI-29";
-  this.enrolled_students = 21;
-  // this.city = "San Francisco Bay Area";
-  // this.state = "California";
-  // this.letters = 13;
-  // this.shout  = function(str) {
-  // 	return str.toUpperCase() + "!";
-  // 	// Where is 'str' coming from
-  // };
+  this.enrolled_students = ["testing", "testing2"];
+  this.start_date = "05/02/2016";
+  this.end_date = "07/22/2016";
+  this.current_date = "07/04/2016";
+  this.shout = function () {
+    var daysRemaining = this.end_date - this.current_date;
+    return daysRemaining; 
+  };
 }
+
+
+
